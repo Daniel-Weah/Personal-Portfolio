d09962f34a1734e29a1c8c9fe6e837c1ce461da5
Daniel K. Weah, Jr <weahjrdanielk@gmail.com>
2024-06-20 14:39:40 +0000

    third commit

--- mail.php
+++ mail.php
@@ -1,33 +1,21 @@
-<?php 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    // collect form data
-    $name = 
-    htmlspecialchars($_POST['name']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
-    $phone =
-    htmlspecialchars($_POST['phone']);
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $to = "weahjrdanielk@gmail.com";
+        $subject = "New Contact Form Submission from Portfolio";
+        $body = "Name: $name\nPhone: $phone\nEmail: $email\n\nMessage:\n$message";
+        $headers = "From: $email";
 
-    $email = 
-    htmlspecialchars($_POST['email']);
-
-    $message = 
-    htmlspecialchars($_POST['message']);
-
-    // Email details to be sent
-    $to = "weahjrdanielk@gmail.com";
-
-    $subject = "New Contact Form Submission from Portfolio";
-
-    $body = "Name: $name\nPnone: $phone\nEmail: $email\n\nMessage: \n $message";
-
-    $headers = "From: $email";
-
-    // send email
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Thank you for contacting me!"
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Thank you for contacting me!";
+        } else {
+            echo "Sorry, something went wrong. Please try again later.";
+        }
     } else {
-        echo "Sorry, something went wrong. Please try again later."
+        echo "Invalid email address.";
     }
-
 }
-?>
